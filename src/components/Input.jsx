4063962d19f39227edc710fa7d34d867f107f364
Roman <roman.markanych@gmail.com>
2023-03-15 01:13:00 +0200

    added material ui

---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+
+const Input = () => {
+  return (
+    <TextField id="standard-basic" label="Enter a country" variant="standard" />
+  )
+}
+
+export default Input
